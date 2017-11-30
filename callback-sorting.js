7e070d162f342afdb4ef7c7e4f6cb235ff53a4e1
Brandon Samuel <brandonsamuel72@gmail.com>
2017-11-30 22:43:26 +0000

    sorting callback functions

---
+++ callback-sorting.js
@@ -0,0 +1,23 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a,b) {
+  var nameA = a.name;
+  var nameB = b.name;
+  var nameAAge = a.age;
+  var nameBAge = b.age;
+  if (nameA === nameB && nameAAge < nameBAge) {
+    return 1;
+  } else if(nameA < nameB) {
+    return -1;
+  } else if (nameA > nameB) {
+    return 1;
+  }
+  return 0;
+});
+
+console.log(students);
